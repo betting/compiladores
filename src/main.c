25bab06494c5f7006db977d5ab47d21288466e66
Frederico Betting <frederico.betting@gmail.com>
2013-08-28 20:12:14 -0300

    [Etapa 02] Adicionando arquivos base para etapa 02.

--- src/main.c
+++ src/main.c
@@ -1,24 +1,22 @@
+/*
+   main.c
+
+   Arquivo principal do analisador sintático.
+*/
 #include <stdio.h>
 #include "comp_dict.h"
 #include "comp_list.h"
 #include "comp_tree.h"
 #include "comp_graph.h"
-#include "tokens.h"
 
-extern int lineNumber;
-
-int getLineNumber (void)
+void yyerror (char const *mensagem)
 {
-  return lineNumber;
+  fprintf (stderr, "%s\n", mensagem);
 }
 
 int main (int argc, char **argv)
 {
-  initDict();
-  int token = TOKEN_ERRO;
-  while (token = yylex())
-  {
-    printf ("token <%d> at %d\n", token, getLineNumber());
-  }
-  return 0;
+  int resultado = yyparse();
+  return resultado;
 }
+
