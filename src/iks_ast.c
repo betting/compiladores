8561337ac85977aa975a1ca2b925fd873c9b03fb
Samuel Silva <samucasilva@gmail.com>
2013-09-15 19:08:19 -0300

    [ETAPA 03] - Criação dos nodos da árvore AST

--- src/iks_ast.c
+++ src/iks_ast.c
@@ -1,16 +1,44 @@
 #include "iks_ast.h"
 
-IKS_AST* criaIKS_AST(int tipo, comp_dict_item_t* simbolo, IKS_AST** filhos, int numFilhos){
+
+/** 
+ * função que cria um novo nodo
+ * 
+ */
+
+IKS_AST* criaIKS_AST(int tipo, comp_dict_item_t* simbolo, IKS_AST** filhos, int numFilhos) {
+		IKS_AST *nodo = (AST*) malloc(sizeof(AST));
+		nodo->tipo = tipo;
+		nodo->numFilhos = numFilhos;
+		nodo->simbolo = simbolo;
+		nodo->filhos = filhos; 
+		return nodo;
+}
+
+/** 
+ * função que cria um novo nodo
+ * 
+ */
+IKS_AST** criaNODOS(IKS_AST* f1, IKS_AST* f2, IKS_AST* f3, IKS_AST* f4, int numFilhos) {
+	int index = 0;
+	IKS_AST** nodos = (IKS_AST**)calloc(numFilhos, sizeof(IKS_AST*));
+	criaNodo(nodos, f1, &index);
+	criaNodo(nodos, f2, &index);
+	criaNodo(nodos, f3, &index);
+	criaNodo(nodos, f4, &index);
+	return nodos;
 }
 
 
-
-IKS_AST** criaNODOS(IKS_AST* f1, IKS_AST* f2, IKS_AST* f3, IKS_AST* f4, int numFilhos){
-
-}
-
-
-void criaNodo(IKS_AST** filhos,IKS_AST* filho, int* index){
+/** 
+ * função que cria nodo e incrementa o índice
+ * 
+ */
+void criaNodo(IKS_AST** filhos,IKS_AST* filho, int* index) {
+	if(filho != NULL) {
+		filhos[(*index)] = filho;
+		(*index)++;
+	}
 
 }
 
