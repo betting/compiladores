f037a63e4a4b17b96ab0fb75afb4e168fd79c355
Frederico Betting <frederico.betting@gmail.com>
2013-09-16 21:55:50 -0300

    Adicionando script de teste.

---
+++ test_errado.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "01"; cat tests/syntax/input/errada01.txt | ./main
+echo 
+echo "02"; cat tests/syntax/input/errada02.txt | ./main
+echo 
+echo "03"; cat tests/syntax/input/errada03.txt | ./main
+echo 
+echo "04"; cat tests/syntax/input/errada04.txt | ./main
+echo 
+echo "05"; cat tests/syntax/input/errada05.txt | ./main
+echo 
+echo "06"; cat tests/syntax/input/errada06.txt | ./main
+echo 
+echo "07"; cat tests/syntax/input/errada07.txt | ./main
+echo 
+echo "08"; cat tests/syntax/input/errada08.txt | ./main
+echo 
+echo "09"; cat tests/syntax/input/errada09.txt | ./main
+echo 
+echo "10"; cat tests/syntax/input/errada10.txt | ./main
+echo 
+echo "11"; cat tests/syntax/input/errada11.txt | ./main
+echo 
+echo "12"; cat tests/syntax/input/errada12.txt | ./main
+echo 
+echo "13"; cat tests/syntax/input/errada13.txt | ./main
+echo 
+echo "14"; cat tests/syntax/input/errada14.txt | ./main
+echo 
+echo "15"; cat tests/syntax/input/errada15.txt | ./main
+echo 
+echo "16"; cat tests/syntax/input/errada16.txt | ./main
+echo 
+echo "17"; cat tests/syntax/input/errada17.txt | ./main
+echo 
+echo "18"; cat tests/syntax/input/errada18.txt | ./main
+echo 
+echo "19"; cat tests/syntax/input/errada19.txt | ./main
+echo 
+echo "20"; cat tests/syntax/input/errada20.txt | ./main
+echo 
+
