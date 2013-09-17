1cafe233ec76669f724d354dfcb125f9f16e6f12
Frederico Betting <frederico.betting@gmail.com>
2013-09-16 22:37:39 -0300

    [Etapa 03] Corrigindo tree e referencias em scanner.l

--- include/comp_tree.h
+++ include/comp_tree.h
@@ -16,7 +16,6 @@ typedef struct _tree
 /* prototipos das funções */ 
 comp_tree_t* search(comp_tree_t* root,int data);
 comp_tree_t* initTree(comp_tree_t* root,int data[]);
-//void createNode(int data);
 comp_tree_t* createNode(int type, comp_dict_item_t* symbol);
-void insertChild(comp_tree_t* parent, comp_tree_t* child)
-void addSiblings(comp_tree_t* first, comp_tree_t* newNode)
+void insertChild(comp_tree_t* parent, comp_tree_t* child);
+void addSiblings(comp_tree_t* first, comp_tree_t* newNode);
