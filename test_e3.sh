93457dc5e173dd10164004f048822729063f228f
Frederico Betting <frederico.betting@gmail.com>
2013-09-29 20:14:17 -0300

    Script de teste.

--- test_e3.sh
+++ test_e3.sh
@@ -1,53 +1,205 @@
 #!/bin/bash
 echo "ETAPA3";
 echo "00"; cat tests/syntax/input/etapa3/i00.iks | ./main
+cp saida.dot result/saida00.dot
+dot saida.dot -Tpng -o result/saida00.png
+
 echo "01"; cat tests/syntax/input/etapa3/i01.iks | ./main
+cp saida.dot result/saida01.dot
+dot saida.dot -Tpng -o result/saida01.png
+
 echo "02"; cat tests/syntax/input/etapa3/i02.iks | ./main
+cp saida.dot result/saida02.dot
+dot saida.dot -Tpng -o result/saida02.png
+
 echo "03"; cat tests/syntax/input/etapa3/i03.iks | ./main
+cp saida.dot result/saida03.dot
+dot saida.dot -Tpng -o result/saida03.png
+
 echo "04"; cat tests/syntax/input/etapa3/i04.iks | ./main
+cp saida.dot result/saida04.dot
+dot saida.dot -Tpng -o result/saida04.png
+
 echo "05"; cat tests/syntax/input/etapa3/i05.iks | ./main
+cp saida.dot result/saida05.dot
+dot saida.dot -Tpng -o result/saida05.png
+
 echo "06"; cat tests/syntax/input/etapa3/i06.iks | ./main
+cp saida.dot result/saida06.dot
+dot saida.dot -Tpng -o result/saida06.png
+
 echo "07"; cat tests/syntax/input/etapa3/i07.iks | ./main
+cp saida.dot result/saida07.dot
+dot saida.dot -Tpng -o result/saida07.png
+
 echo "08"; cat tests/syntax/input/etapa3/i08.iks | ./main
+cp saida.dot result/saida08.dot
+dot saida.dot -Tpng -o result/saida08.png
+
 echo "09"; cat tests/syntax/input/etapa3/i09.iks | ./main
+cp saida.dot result/saida09.dot
+dot saida.dot -Tpng -o result/saida09.png
+
 echo "10"; cat tests/syntax/input/etapa3/i10.iks | ./main
+cp saida.dot result/saida10.dot
+dot saida.dot -Tpng -o result/saida10.png
+
 echo "11"; cat tests/syntax/input/etapa3/i11.iks | ./main
+cp saida.dot result/saida11.dot
+dot saida.dot -Tpng -o result/saida11.png
+
 echo "12"; cat tests/syntax/input/etapa3/i12.iks | ./main
+cp saida.dot result/saida12.dot
+dot saida.dot -Tpng -o result/saida12.png
+
 echo "13"; cat tests/syntax/input/etapa3/i13.iks | ./main
+cp saida.dot result/saida13.dot
+dot saida.dot -Tpng -o result/saida13.png
+
 echo "14"; cat tests/syntax/input/etapa3/i14.iks | ./main
+cp saida.dot result/saida14.dot
+dot saida.dot -Tpng -o result/saida14.png
+
 echo "15"; cat tests/syntax/input/etapa3/i15.iks | ./main
+cp saida.dot result/saida15.dot
+dot saida.dot -Tpng -o result/saida15.png
+
 echo "16"; cat tests/syntax/input/etapa3/i16.iks | ./main
+cp saida.dot result/saida16.dot
+dot saida.dot -Tpng -o result/saida16.png
+
 echo "17"; cat tests/syntax/input/etapa3/i17.iks | ./main
+cp saida.dot result/saida17.dot
+dot saida.dot -Tpng -o result/saida17.png
+
 echo "18"; cat tests/syntax/input/etapa3/i18.iks | ./main
+cp saida.dot result/saida18.dot
+dot saida.dot -Tpng -o result/saida18.png
+
 echo "19"; cat tests/syntax/input/etapa3/i19.iks | ./main
+cp saida.dot result/saida19.dot
+dot saida.dot -Tpng -o result/saida19.png
+
 echo "20"; cat tests/syntax/input/etapa3/i20.iks | ./main
+cp saida.dot result/saida20.dot
+dot saida.dot -Tpng -o result/saida20.png
+
 echo "21"; cat tests/syntax/input/etapa3/i21.iks | ./main
+cp saida.dot result/saida00.dot
+dot saida.dot -Tpng -o result/saida00.png
+
 echo "22"; cat tests/syntax/input/etapa3/i22.iks | ./main
+cp saida.dot result/saida22.dot
+dot saida.dot -Tpng -o result/saida22.png
+
 echo "23"; cat tests/syntax/input/etapa3/i23.iks | ./main
+cp saida.dot result/saida23.dot
+dot saida.dot -Tpng -o result/saida23.png
+
 echo "24"; cat tests/syntax/input/etapa3/i24.iks | ./main
+cp saida.dot result/saida24.dot
+dot saida.dot -Tpng -o result/saida24.png
+
 echo "25"; cat tests/syntax/input/etapa3/i25.iks | ./main
+cp saida.dot result/saida25.dot
+dot saida.dot -Tpng -o result/saida25.png
+
 echo "26"; cat tests/syntax/input/etapa3/i26.iks | ./main
+cp saida.dot result/saida26.dot
+dot saida.dot -Tpng -o result/saida26.png
+
 echo "27"; cat tests/syntax/input/etapa3/i27.iks | ./main
+cp saida.dot result/saida27.dot
+dot saida.dot -Tpng -o result/saida27.png
+
 echo "28"; cat tests/syntax/input/etapa3/i28.iks | ./main
+cp saida.dot result/saida28.dot
+dot saida.dot -Tpng -o result/saida28.png
+
 echo "29"; cat tests/syntax/input/etapa3/i29.iks | ./main
+cp saida.dot result/saida29.dot
+dot saida.dot -Tpng -o result/saida29.png
+
 echo "30"; cat tests/syntax/input/etapa3/i30.iks | ./main
+cp saida.dot result/saida30.dot
+dot saida.dot -Tpng -o result/saida30.png
+
 echo "31"; cat tests/syntax/input/etapa3/i31.iks | ./main
+cp saida.dot result/saida31.dot
+dot saida.dot -Tpng -o result/saida31.png
+
 echo "32"; cat tests/syntax/input/etapa3/i32.iks | ./main
+cp saida.dot result/saida32.dot
+dot saida.dot -Tpng -o result/saida32.png
+
 echo "33"; cat tests/syntax/input/etapa3/i33.iks | ./main
+cp saida.dot result/saida33.dot
+dot saida.dot -Tpng -o result/saida33.png
+
 echo "34"; cat tests/syntax/input/etapa3/i34.iks | ./main
+cp saida.dot result/saida34.dot
+dot saida.dot -Tpng -o result/saida34.png
+
 echo "35"; cat tests/syntax/input/etapa3/i35.iks | ./main
+cp saida.dot result/saida35.dot
+dot saida.dot -Tpng -o result/saida35.png
+
 echo "36"; cat tests/syntax/input/etapa3/i36.iks | ./main
+cp saida.dot result/saida36.dot
+dot saida.dot -Tpng -o result/saida36.png
+
 echo "37"; cat tests/syntax/input/etapa3/i37.iks | ./main
+cp saida.dot result/saida37.dot
+dot saida.dot -Tpng -o result/saida37.png
+
 echo "38"; cat tests/syntax/input/etapa3/i38.iks | ./main
+cp saida.dot result/saida38.dot
+dot saida.dot -Tpng -o result/saida38.png
+
 echo "39"; cat tests/syntax/input/etapa3/i39.iks | ./main
+cp saida.dot result/saida39.dot
+dot saida.dot -Tpng -o result/saida39.png
+
 echo "40"; cat tests/syntax/input/etapa3/i40.iks | ./main
+cp saida.dot result/saida40.dot
+dot saida.dot -Tpng -o result/saida40.png
+
 echo "41"; cat tests/syntax/input/etapa3/i41.iks | ./main
+cp saida.dot result/saida41.dot
+dot saida.dot -Tpng -o result/saida41.png
+
 echo "42"; cat tests/syntax/input/etapa3/i42.iks | ./main
+cp saida.dot result/saida42.dot
+dot saida.dot -Tpng -o result/saida42.png
+
 echo "43"; cat tests/syntax/input/etapa3/i43.iks | ./main
+cp saida.dot result/saida43.dot
+dot saida.dot -Tpng -o result/saida43.png
+
 echo "44"; cat tests/syntax/input/etapa3/i44.iks | ./main
+cp saida.dot result/saida44.dot
+dot saida.dot -Tpng -o result/saida44.png
+
 echo "45"; cat tests/syntax/input/etapa3/i45.iks | ./main
+cp saida.dot result/saida45.dot
+dot saida.dot -Tpng -o result/saida45.png
+
 echo "46"; cat tests/syntax/input/etapa3/i46.iks | ./main
+cp saida.dot result/saida46.dot
+dot saida.dot -Tpng -o result/saida46.png
+
 echo "47"; cat tests/syntax/input/etapa3/i47.iks | ./main
+cp saida.dot result/saida47.dot
+dot saida.dot -Tpng -o result/saida47.png
+
 echo "48"; cat tests/syntax/input/etapa3/i48.iks | ./main
+cp saida.dot result/saida48.dot
+dot saida.dot -Tpng -o result/saida48.png
+
 echo "49"; cat tests/syntax/input/etapa3/i49.iks | ./main
+cp saida.dot result/saida49.dot
+dot saida.dot -Tpng -o result/saida49.png
+
 echo "50"; cat tests/syntax/input/etapa3/i50.iks | ./main
+cp saida.dot result/saida50.dot
+dot saida.dot -Tpng -o result/saida50.png
