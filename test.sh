0c6ebf28cd5cc27ca5219692135a23d224c92ebb
Frederico Betting <frederico.betting@gmail.com>
2013-11-10 19:06:21 -0200

    [Etapa 05] Alterando script de execução de testes.

--- test.sh
+++ test.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-clear
-echo "Executando testes..."
-echo
-echo "-------------------------------------------------"
-echo "1. Comentarios"
-cat tests/syntax/test01_comment.txt | ./main
 
-echo "-------------------------------------------------"
-echo "2. Variaveis"
-cat tests/syntax/test02_var.txt | ./main
-
-echo "-------------------------------------------------"
-echo "3. Funcoes"
-cat tests/syntax/test03_functions.txt | ./main
-
-echo "-------------------------------------------------"
-echo "4. Atribuicoes"
-cat tests/syntax/test04_attrb.txt | ./main
-
-echo "-------------------------------------------------"
-echo "93. Funcoes (Erro esperado: linha 18)"
-cat tests/syntax/test93_functions_err.txt | ./main
-
-echo "-------------------------------------------------"
-echo "94. Atribuicoes (Erro esperado: linha 9)"
-cat tests/syntax/test94_attrb_err.txt | ./main
-
-echo "-------------------------------------------------"
+DIR="tests/etapa05/"
+for ARQUIVO in `ls -1 $DIR`
+do
+   echo $ARQUIVO
+   ./main $DIR$ARQUIVO
+done
